8378a6040515ffaf6ea47c96b4c9ff4c79714f21
Garry <yinfo@mail.ru>
2018-04-11 23:33:14 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,9 @@ import {TransferHttpCacheModule} from '@nguniversal/common';
  import {HttpClientModule} from '@angular/common/http';
  import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
  import {FlexLayoutModule} from '@angular/flex-layout';
- // import {
-     // MatButtonModule,
-    // MatCardModule,
+  import {
+      MatButtonModule,
+     MatCardModule,
 //     MatCheckboxModule,
 //     MatDialogModule,
 //     MatIconModule,
@@ -25,7 +25,7 @@ import {TransferHttpCacheModule} from '@nguniversal/common';
 //     MatSlideToggleModule,
 //     MatTabsModule,
 //     MatToolbarModule
- // } from '@angular/material';
+  } from '@angular/material';
 //
  // import 'hammerjs';
 
@@ -47,8 +47,8 @@ import {TransferHttpCacheModule} from '@nguniversal/common';
          HttpClientModule,
          BrowserAnimationsModule,
         // // Material
-         // MatButtonModule,
-         // MatCardModule,
+          MatButtonModule,
+          MatCardModule,
         // MatCheckboxModule,
         // MatDialogModule,
         // MatIconModule,

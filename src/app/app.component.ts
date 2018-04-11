49e7e2455c830a714bd03c4dd3f2ff88039a8b81
Garry <yinfo@mail.ru>
2018-04-11 20:07:44 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-  <h1>Universal Demo using Angular and Angular CLI</h1>
-  <a routerLink="/">Home</a>
-  <a routerLink="/lazy">Lazy</a>
-  <a routerLink="/lazy/nested">Lazy_Nested</a>
-  <router-outlet></router-outlet>
-  `,
-  styles: []
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+    users = [
+        {
+            name: 'Lia Lugo',
+            avatar: 'svg-11',
+            details: 'I love cheese, ...',
+            isAdmin: true,
+            isCool: false
+        },
+        {
+            name: 'George Duke',
+            avatar: 'svg-12',
+            details: 'Zombie ipsum ...',
+            isAdmin: false,
+            isCool: true
+        }
+        // ...
+    ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,72 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 import {TransferHttpCacheModule} from '@nguniversal/common';
 
+// material
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatTabsModule,
+    MatToolbarModule
+} from '@angular/material';
+
+import 'hammerjs';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
-  imports: [
-    BrowserModule.withServerTransition({appId: 'my-app'}),
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full'},
-      { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
-      { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
-    ]),
-    TransferHttpCacheModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+    ],
+    imports: [
+        BrowserModule.withServerTransition({appId: 'my-app'}),
+        RouterModule.forRoot([
+            {path: '', component: HomeComponent, pathMatch: 'full'},
+            {path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
+            {path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
+        ]),
+        TransferHttpCacheModule,
+        // For material
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        // Material
+        MatButtonModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatDialogModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSlideToggleModule,
+        MatTabsModule,
+        MatToolbarModule,
+
+        // Flex-layout
+        FlexLayoutModule
+
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
